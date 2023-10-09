e9cb12abc9d558d2966218fd2fe90f746c88dc52
Netcode-Hub <Rastinga1995@hotmail.com>
2023-10-09 11:57:26 +0000

    Add project files.

---
+++ DemoHideShowBlazorChildComponents/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoHideShowBlazorChildComponents.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ DemoHideShowBlazorChildComponents/Models/ItemService.cs
@@ -0,0 +1,35 @@
+﻿namespace DemoHideShowBlazorChildComponents.Models
+{
+    public interface IItemService
+    {
+        List<Item> GetItems();
+        Item GetItem(int id);
+        void UpdateItem(Item model);
+        void AddItem(Item i);
+    }
+
+    public class ItemService : IItemService
+    {
+        private List<Item> Items = new();
+
+        public List<Item> GetItems() => Items;
+        public Item GetItem(int id) => Items.FirstOrDefault(_ => _.Id == id);
+
+        public void UpdateItem(Item model)
+        {
+            Item i = Items.FirstOrDefault(_ => _.Id == model.Id);
+            i.Name = model.Name; i.Quantity = model.Quantity;
+        }
+
+        public void AddItem(Item i)
+        {
+            Items.Add(i);
+        }
+
+        public void DeleteItem(int id)
+        {
+            Items.Remove(Items.FirstOrDefault(_ => _.Id == id));
+        }
+    }
+
+}
